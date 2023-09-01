79e0b3b95e7bdd1ac6e85433bc1f12195e445e33
SOPORTE <SOPORTE@SALA1-14-BAQ.unilibre.edu.co>
2023-09-01 11:50:37 -0500

    cambios

--- bin/parcial.dart
+++ bin/parcial.dart
@@ -1,5 +1,68 @@
-import 'package:parcial/parcial.dart' as parcial;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${parcial.calculate()}!');
+class Producto {
+  String nombre;
+  double precio;
+  DateTime fechaVencimiento;
+  Producto(this.nombre, this.precio, this.fechaVencimiento);
+}
+
+class Ventas {
+  List<Producto> productos;
+
+  Ventas() : productos = [] {
+    //constructor
+  }
+
+  void agregarProducto(Producto producto) {
+    this.productos.add(producto);
+  }
+
+  double obtenerTotal() {
+    double total = 0.0;
+    for (Producto producto in this.productos) {
+      if (producto.fechaVencimiento.difference(DateTime.now()).inDays <= 15) {
+        total += producto.precio * 0.85;
+      } else {
+        total += producto.precio;
+      }
+    }
+    return total;
+  }
+  
+}
+
+
+
+void main() {
+  Ventas ventas = Ventas();
+
+
+  print("Ingrese el nombre del producto 1: ");
+  String nombreProducto1 = stdin.readLineSync()!;
+  print("Ingrese el precio del producto 1: ");
+  double precioProducto1 = double.parse(stdin.readLineSync()!);
+  print("Ingrese la fecha de vencimiento del producto 1 (YYYY-MM-DD): ");
+  String fechaVencimientoProducto1 = stdin.readLineSync()!;
+
+  Producto producto1 = Producto(nombreProducto1, precioProducto1, DateTime.parse(fechaVencimientoProducto1));
+
+/*
+  print("Ingrese el nombre del producto 2: ");
+  String nombreProducto2 = stdin.readLineSync()!;
+  print("Ingrese el precio del producto 2: ");
+  double precioProducto2 = double.parse(stdin.readLineSync()!);
+  print("Ingrese la fecha de vencimiento del producto 2 (YYYY-MM-DD): ");
+  String fechaVencimientoProducto2 = stdin.readLineSync()!;
+
+  Producto producto2 = Producto(nombreProducto2, precioProducto2, DateTime.parse(fechaVencimientoProducto2));
+*/
+
+  ventas.agregarProducto(producto1);
+  //ventas.agregarProducto(producto2);
+
+
+  double total = ventas.obtenerTotal();
+
+  print("El total es: $total");
 }
